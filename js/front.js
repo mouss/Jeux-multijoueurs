368e17d863bdd8152016dca22eca3955ca71eaa5
mouss <mouss_78@hotmail.com>
2017-04-05 23:46:33 +0200

    Update front.js
    ip change

--- js/front.js
+++ js/front.js
@@ -6,7 +6,7 @@ window.addEventListener('load',function(){
 
     var j = 0;
 
-    var socket = io('http://54.93.237.237:8080/');
+    var socket = io('http://127.0.0.1:8080/');
 
     var pseudo;
 
